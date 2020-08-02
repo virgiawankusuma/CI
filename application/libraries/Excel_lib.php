2f4db0d298bf0572f2ca194b347e031f45814b6c
virgiawankusuma <virgiawan150@gmail.com>
2020-08-02 19:58:25 +0700

    nambah excel lib

--- application/libraries/Excel_lib.php
+++ application/libraries/Excel_lib.php
@@ -1,6 +1,7 @@
 <?php if(!defined('BASEPATH')) die('No Access');
 require_once APPPATH."/third_party/PHPExcel.php";
 require_once APPPATH."/third_party/PHPExcel/IOFactory.php";
+require_once APPPATH."/third_party/PHPExcel/Writer/Excel2007.php";
 
 /**
  * 
